7075728ee5871ede2f934a151efd4dd2c7a06971
virtualheet <heetvavadiya456@gmail.com>
2025-06-18 19:56:39 +0530

    feat: add project and work images
    - Added images to the projects section.
    - Added images to the work section.
    - Added fonts.css for custom fonts.
    - Created components directory.
    - Updated project and work pages.

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LanyardResponse } from 'react-use-lanyard'
+
+const Home = ({ activity }: { activity: LanyardResponse | undefined }) =>  {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
