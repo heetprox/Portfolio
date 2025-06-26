a8ce02fbb2df68e06a65e730f05b1e89b0e82f9e
virtualheet <heetvavadiya456@gmail.com>
2025-06-26 21:55:07 +0530

    feat: add lucide-react dependency and update project layout
    - Added `lucide-react` as a dependency for icon usage.
    - Updated Navbar component for improved layout and responsiveness.
    - Enhanced page layout with adjustments to flex properties and added a new image section for project display.
    - Modified global styles to include a border for better visual structure.

--- src/app/(root)/[project]/page.tsx
+++ src/app/(root)/[project]/page.tsx
@@ -19,23 +19,23 @@ const page = () => {
                 gap: "clamp(0.5rem, 2.25vw, 240rem)",
             }}>
             {/* {data.map((project, index) => ( */}
-            <div className="flex w-full bg-[#131313] h-[90vh]">
+            <div className="flex w-full bg-[#131313] max-h-[90vh] h-auto    ">
                 <Image
                     src="/projects/vaayujewels.svg"
-                    className='w-full h-[90vh] object-cover'
+                    className='w-full max-h-[90vh] h-auto object-cover'
                     alt="yo"
                     width={100}
                     height={100}
                 />
             </div>
 
-            <div className="flex min-h-[200vh]  w-full h-full" style={{
+            <div className="flex min-h-[200vh] flex-col md:flex-row  w-full h-full" style={{
                 position: 'relative',
                 gap: "clamp(0.5rem, 0.75vw, 240rem)",
 
             }}>
 
-                <div className="w-[33%]"
+                <div className="w-full md:w-[33%]"
                     style={{
                         position: 'sticky',
                         top: "clamp(0.5rem, 4vw, 240rem)",
@@ -61,37 +61,37 @@ const page = () => {
                 </div>
 
 
-                <div className="w-[67%] flex flex-col" style={{
+                <div className="w-full md:w-[67%] flex flex-col" style={{
                     minHeight: '200vh',
                     gap: "clamp(0.5rem, 0.75vw, 240rem)",
                     fontSize: "clamp(0.75rem, 0.9vw, 240rem)",
 
                 }}
                 >
-                    <div className="flex w-full"
+                    <div className="flex w-full flex-col md:flex-row"
                         style={{
                             gap: "clamp(0.5rem, 0.75vw, 240rem)",
                         }}
                     >
-                        <div className="flex w-[50%] flex-col">
+                        <div className="flex w-full md:w-[50%] flex-col">
                             <p className="mb-4  leading-tight" >
                                 Jo (He/Him) designs interfaces. He thrives in complex, ambiguous problem spaces focused around interactive media, digital tooling, and multimodal interaction.  If you have an interesting idea, please get in touch↗.
                             </p>
                         </div>
 
-                        <div className="flex w-[50%] flex-col">
+                        <div className="md:flex md:w-[50%] hidden w-full flex-col">
                             <p className="mb-4  leading-tight" >
                                 Jo (He/Him) designs interfaces. He thrives in complex, ambiguous problem If you have an interesting idea, please get in touch↗.
                             </p>
                         </div>
                     </div>
 
-                    <div className="flex w-full"
+                    <div className="grid grid-cols-2 md:flex w-full"
                         style={{
                             gap: "clamp(0.5rem, 0.75vw, 240rem)",
                         }}
                     >
-                        <div className="flex flex-col w-1/4"
+                        <div className="flex flex-col w-full md:w-1/4"
 
                         >
                             <h3
@@ -111,7 +111,7 @@ const page = () => {
 
 
 
-                        <div className="flex flex-col w-1/4"
+                        <div className="flex flex-col w-full md:w-1/4"
 
                         >
                          <h3
@@ -130,7 +130,7 @@ const page = () => {
 
 
 
-                        <div className="flex flex-col w-1/4"
+                        <div className="flex flex-col w-full md:w-1/4"
 
                         >
                             <h3
@@ -149,7 +149,7 @@ const page = () => {
 
 
 
-                        <div className="flex flex-col w-1/4"
+                        <div className="flex flex-col w-full md:w-1/4"
 
                         >
                             <h3
@@ -160,7 +160,7 @@ const page = () => {
                                 className="uppercase mono tracking-wide font-medium text-white/40">
                                 TOOLS
                             </h3>
-                            <p className="mb-4  leading-tight" >
+                            <p className="mb-4   leading-tight" >
                                 Next.js (App Router) (Api).
                                 <br />
                                 Framer-motion.
@@ -170,37 +170,60 @@ const page = () => {
                         </div>
                     </div>
 
-                    <div className="flex"
+                    <div className="flex md:flex-row flex-col"
                     style={{
                         gap: "clamp(0.5rem, 0.75vw, 240rem)",
                         paddingTop: "clamp(0.5rem, 1.75vw, 240rem)",
                     }}
                     >
 
-                        <div className="flex flex-col w-1/2">
+                        <div className="flex flex-col md:w-1/2 w-full">
                             <button
                                 style={{
                                     fontSize: "clamp(0.75rem, 0.75vw, 240rem)",
-                                    padding: "clamp(0.25rem, 0.75vw, 240rem)",
+                                    padding: "clamp(1rem, 0.75vw, 240rem)",
                                 }}
-                                className="uppercase mono text-left font-normal flex justify-between text-white/70 bg-neutral-400/5 rounded-md cursor-pointer hover:bg-white/5 transition-all duration-100 hover:text-white">
+                                className="uppercase mono text-left font-normal flex justify-between text-white/70 bg-neutral-300/5 rounded-md cursor-pointer hover:bg-neutral-200/10 transition-all duration-100 hover:text-white">
                                     <div>MAIN WEBSITE</div>
                                     <div className="scale-150">↗</div>
                             </button>
                            
                         </div>
-                        <div className="flex flex-col w-1/2">
+                        <div className="flex flex-col md:w-1/2 w-full">
                             <button
                                 style={{
                                     fontSize: "clamp(0.75rem, 0.75vw, 240rem)",
-                                    padding: "clamp(0.25rem, 0.75vw, 240rem)",
+                                    padding: "clamp(1rem, 0.75vw, 240rem)",
                                 }}
-                                className="uppercase mono text-left   tracking-widest font-normal flex justify-between text-white/70 bg-neutral-400/5 rounded-md cursor-pointer hover:bg-white/5 transition-all duration-100 hover:text-white">
+                                className="uppercase mono text-left font-normal flex justify-between text-white/70 bg-neutral-300/5 rounded-md cursor-pointer hover:bg-neutral-200/10 transition-all duration-100 hover:text-white">
                                     <div>SEO</div>
                                     <div className="scale-150">↗</div>
                             </button>
                            
                         </div>
+                    </div>
+
+
+                    <div className="flex flex-col w-full h-full  "
+                    style={{
+                        paddingTop: "clamp(0.5rem, 0.75vw, 240rem)",
+                    }}
+                    >
+                        <Image
+                            src="/projects/vaayo.png"
+                            className=' h-auto w-full object-cover '
+                            alt="IF you are not seeing image this than you shouln't be."
+                            width={100}
+                            height={100}
+                        />
+                          <h3
+                                style={{
+                                    fontSize: "clamp(0.75rem, 0.75vw, 240rem)",
+                                    paddingTop: "clamp(0.25rem, 0.75vw, 240rem)",
+                                }}
+                                className="uppercase mono tracking-wide font-medium text-white/50">
+                                Multimodal search with the ability to give follow-up queries.
+                            </h3>
                     </div>
                 </div>
             </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,37 @@
+'use client'
+import { X } from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
+
+    const pathname = usePathname()
+    const isHome = pathname === '/'
     return (
-        <nav className="sticky top-0 left-0 border-b border-white/10 w-full flex justify-between items-center  text-white z-50"
+        <nav className="sticky top-0 left-0 border-b bg-[#131313] border-white/10 w-full flex justify-between items-center  text-white z-50"
             style={{
                 fontSize: "clamp(0.8rem, 0.9vw, 240rem)",
                 padding: "clamp(0.75rem, 0.75vw, 240rem) clamp(0.5rem, 0.75vw, 240rem)",
             }}
         >
-            <div className="flex w-1/2">
+            <div className="flex w-[35%] md:w-1/2">
                 <Link href="/" className="text-left">
                     <h1 className=" tracking-tight">Heet Vavadiya</h1>
                 </Link>
             </div>
-            <div className="flex w-1/2 justify-between">
-                <div className="text-left w-[35%]">
+            <div className="flex w-[65%] md:w-1/2 md:justify-start justify-between">
+                <div className="text-left md:w-[35%]">
                     <h2 className=" ">Full-Stack Developer</h2>
                 </div>
-                <div className="text-left w-[65%]">
+                <div className="md:flex hidden text-left w-[55%]">
                     <p className=" ">Ahmedabad, In</p>
                 </div>
+                {!isHome && (
+                    <Link href="/" className="flex text-white cursor-pointer w-[10%] justify-end">
+                        <X className="text-white"/>
+                    </Link>
+                )}
             </div>
         </nav>
     )
