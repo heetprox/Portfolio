50f09dedfb163ea8265f5e0105a75d65c7b39e84
virtualheet <heetvavadiya456@gmail.com>
2025-07-25 08:07:07 +0530

    refactor: improve layout and styling in Home component
    - Adjusted layout styles for better spacing and alignment in the Home component.
    - Enhanced the presentation of team data by modifying table styles and ensuring consistent font sizes.
    - Cleaned up unnecessary inline styles for improved readability and maintainability.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -32,67 +32,62 @@ const Home = ({ activity }: { activity: LanyardResponse | undefined }) => {
 
         <div className="w-full flex flex-col md:flex-row
         "
-        style={{
-          gap: "clamp(1rem, 1vw, 240rem)",
-        }}
+          style={{
+            gap: "clamp(1rem, 1vw, 240rem)",
+          }}
         >
           <div className="flex flex-col md:w-[50%] w-full"
-          style={{
-            gap: "clamp(0.5rem, 1vw, 240rem)",
-          }}
+            style={{
+              gap: "clamp(0.5rem, 1vw, 240rem)",
+            }}
           >
             <h3
               style={{
-               fontSize: "clamp(0.75rem, 0.7vw, 240rem)",
-           
+                fontSize: "clamp(0.75rem, 0.7vw, 240rem)",
+
               }}
               className="uppercase mono font-medium mb-4 text-white/40">About</h3>
             <p className="mb-4 md:w-[70%] w-full leading-tight" >
-           {" (He/Him) is a full-stack developer who builds scalable applications people actually use. He loves crafting creative, intuitive frontends, while architecting robust backends that scale. Passionate about real-world impact, he’s constantly exploring new ideas—whether it's for smoother user experiences or pushing boundaries with emerging tech. He’s currently studying Computer Engineering at VGEC, Ahmedabad. If you have an interesting idea, please   "} <Link href="mailto:heetprox@gmail.com" className='hover:text-[#FDE037] hover:border-b hover:border-[#FDE037] border-dotted cursor-pointer'>get in touch↗.</Link>
+              {" (He/Him) is a full-stack developer who builds scalable applications people actually use. He loves crafting creative, intuitive frontends, while architecting robust backends that scale. Passionate about real-world impact, he’s constantly exploring new ideas—whether it's for smoother user experiences or pushing boundaries with emerging tech. He’s currently studying Computer Engineering at VGEC, Ahmedabad. If you have an interesting idea, please   "} <Link href="mailto:heetprox@gmail.com" className='hover:text-[#FDE037] hover:border-b hover:border-[#FDE037] border-dotted cursor-pointer'>get in touch↗.</Link>
             </p>
-           
+
             <div className=" p-4 w-fit">
               <ActivityCard userId="1118212847613247558" initialData={activity} />
             </div>
           </div>
 
           <div className="md:w-[50%] w-full">
-            <div style={{ display: "flex", width: "100%" }}>
+            <div style={{ display: "flex", width: "100%",
+                fontSize: "clamp(0.75rem, 0.7vw, 240rem)",
+             }}>
               <div style={{ width: "35%" }}>
                 <h3
-                  style={{
-                   fontSize: "clamp(0.75rem, 0.7vw, 240rem)",
-                    paddingBottom: "clamp(0.5rem, 1vw, 240rem)",
-                  }}
                   className="uppercase mono tracking-wide font-medium mb-4 text-white/40">TEAM</h3>
               </div>
 
               <div style={{ width: "65%" }}>
                 <h3
-                  style={{
-                   fontSize: "clamp(0.75rem, 0.7vw, 240rem)",
-                    paddingBottom: "clamp(0.5rem, 0.75vw, 240rem)",
-                  }}
                   className="uppercase mono tracking-wide font-medium mb-4 text-white/40">DESCRIPTION</h3>
               </div>
             </div>
 
             <table
-              className="w-full"
+              className="w-full border-separate"
               cellPadding="0"
+              style={{
+                borderSpacing: '0 clamp(0.5rem, 1vw, 240rem)'
+              }}
             >
               <tbody>
                 {teamData.map((entry, index) => (
-                  <tr key={index}>
+                  <tr key={index} className=''>
                     <td className="align-top w-[35%] leading-tight" style={{ verticalAlign: "top" }}>
                       <h4>{entry.company}</h4>
-                      {entry.period && <p style={{
-                        paddingBottom: "clamp(0.5rem, 1vw, 240rem)",
-                      }}>{entry.period}</p>}
+                      {entry.period && <p>{entry.period}</p>}
                     </td>
-                    <td className="align-top w-[65%]" style={{ verticalAlign: "top" }}>
+                    <td className="align-top w-[65%] " style={{ verticalAlign: "top" }}>
                       {entry.superscripts ? (
-                        <p dangerouslySetInnerHTML={{ __html: insertSuperscripts(entry.description, entry.superscripts) }} />
+                        <p className='pb-2' dangerouslySetInnerHTML={{ __html: insertSuperscripts(entry.description, entry.superscripts) }} />
                       ) : (
                         <p className='leading-tight'>{entry.description}</p>
                       )}
