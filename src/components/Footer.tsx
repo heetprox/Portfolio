a98e3a0fd67c8ef7bd28dc676a4853e8a954adb8
virtualheet <heetvavadiya456@gmail.com>
2025-07-26 20:37:03 +0530

    v1.0.3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -79,7 +79,7 @@ const Footer = () => {
                 padding: "clamp(0.25rem, 0.25vw, 20rem) clamp(0.5rem, 0.5vw, 20rem)",
               }}
             >
-              {"v1.0.2"}
+              {"v1.0.3"}
             </div>
             <div className="flex text-white/50 mono scale-90 tracking-tight uppercase">
               Last updated 2025-07-26
