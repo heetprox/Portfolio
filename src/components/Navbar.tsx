8b43c7881fe0c47f53053de81170db5761b9c0a2
virtualheet <heetvavadiya456@gmail.com>
2025-10-02 21:29:28 +0530

    v1.0.9.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,19 +11,19 @@ const Navbar = () => {
     return (
         <nav className="sticky top-0 left-0 border-b bg-[#131313] border-white/10 w-full flex justify-between items-center  text-white z-[999]"
             style={{
-                fontSize: "clamp(0.8rem, 0.9vw, 240rem)",
+                fontSize: "clamp(0.85rem, 0.9vw, 240rem)",
                 padding: "clamp(0.75rem, 0.75vw, 240rem) clamp(0.75rem, 0.75vw, 240rem)",
                 gap: "clamp(1rem, 1vw, 240rem)",
             }}
         >
             <div className="flex relative z-[9999] w-[35%] md:w-1/2">
                 <Link href="/" className="text-left">
-                    <h1 className=" tracking-tight mono">Heet <span className='mono'>0xheet</span> 0xheet</h1>
+                    <h1 className=" tracking-tight">Heet <span className='mono'>{"( 0xheet.eth )"}</span></h1>
                 </Link>
             </div>
             <div className="flex w-[65%] md:w-1/2 md:justify-start justify-between">
                 <div className="text-left w-[70%] md:w-[35%] items-center flex">
-                    <h2 className=" ">Full-Stack Developer</h2>
+                    <h2 className=" ">Developer</h2>
                 </div>
                 <div className="md:flex hidden text-left w-[55%]">
                     <p className=" ">Ahmedabad, In</p>
